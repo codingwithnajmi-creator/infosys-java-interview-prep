d94db65aadeb4355d07a1ed6c1d9814ec9667b3f
Syed <codingwithnajmi@gmail.com>
2025-12-22 17:36:52 -0600

    Fix base package name to com.infosys.interview.preparation

--- core-java-service/src/main/java/com/infosys/interview/preparation/CoreJavaServiceApplication.java
+++ core-java-service/src/main/java/com/infosys/interview/preparation/CoreJavaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.infosys.interview.preperation;
+package com.infosys.interview.preparation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
